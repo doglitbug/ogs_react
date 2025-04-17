4bc7ccf002f961385398a29c8bb062848bffc460
Arron Dick <doglitbug@gmail.com>
2025-04-17 20:33:25 +1200

    Adjustments to allow building and running on dev server

--- app/root.tsx
+++ app/root.tsx
@@ -59,7 +59,7 @@ export function Layout({children}: { children: React.ReactNode }) {
             <Meta/>
             <Links/>
         </head>
-        <body>
+        <body className="body">
         {children}
         <ScrollRestoration/>
         <Scripts/>
